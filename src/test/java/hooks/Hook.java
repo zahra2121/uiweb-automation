5979cb6a7803d346550409d3c64659b326068f58
zahra2121 <hikmatuzzahra21@gmail.com>
2025-07-04 14:10:51 +0700

    Update hook

--- src/test/java/hooks/Hook.java
+++ src/test/java/hooks/Hook.java
@@ -2,7 +2,6 @@ package hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -17,20 +16,15 @@ public class Hook {
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
 
-        // Tambahkan log sebelum membuka browser
-        System.out.println("Launching Chrome headless...");
-
-        WebDriverManager.chromedriver().setup(); // jika kamu pakai WebDriverManager
         driver = new ChromeDriver(options);
-
-
     }
 
     @After
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
